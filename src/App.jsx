a3891384c8face4dae7f3db7c25f7bbc4fc43fbf
vishwas <vishwasbalkundi06@gmail.com>
2025-04-02 15:12:19 +0530

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,20 @@ const App = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col bg-gray-400 text-white">
-      <div className="flex">
+    <div className="w-full h-screen flex flex-col bg-gray-400 text-white">
+      {/* <div className="flex"> */}
         {/* <div className="">
           <SidebarWithSearch></SidebarWithSearch>
         </div> */}
-        <div className="w-full flex flex-col">
+        {/* <div className="w-full flex flex-col"> */}
           <header className="bg-gray-200 p-4 text-center text-xl font-bold text-gray-700">
             T-GPT
           </header>
           <ChatBox className="row-start-1" text={text} />
           <ChatInput className="mt-auto row-end-1" onSend={handleSend} />
-        </div>
+        {/* </div> */}
       </div>
-    </div>
+    // </div>
   );
 };
 
